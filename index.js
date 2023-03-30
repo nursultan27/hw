3081d433c3cd3a63cb109768fe1215a7142f4c06
нурсултан <nuriknurik277551@MacBook-Air-nursultan.local>
2023-03-30 17:43:31 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,99 @@
+// let name = 'hello i am nurs'
+// alert(name)
+
+// let howare('kak tvoi dela?','ne otvetil')
+// console.log(howare);
+// console.log(confirm("Ты занешь JS?"));
+// console.log(!false)
+// let test = false || true
+// // console.log(test);
+// let name = 'nursultan'
+// console.log('nurs')
+// let name = 'nursultan'
+// console.log(name)
+// let age = 30;
+// console.log(age)
+// let age = 30;
+// let isadult = age >= 18;
+// console.log(isadult)
+
+// 1,2 zadanie
+// let Name = 'nursultan'
+// console.log('name '+ Name);
+// let age = '30'
+// console.log('vozrast '+ age);
+// console.log(Name+age)
+
+// let weather = prompt('kakaya pogoda?')
+// if(weather == 'xoroshaya' || 'otlichnaya' ){
+//    alert('kruto')
+// } else {
+//     alert('pechalka')
+// }
+
+//3
+// let isAdult = 30
+// if(isAdult <= 18){
+//     alert('menshe 18')
+// }else{
+//     alert('больше 18')
+// }
+
+// //4
+// let name = prompt("kak tebya zavut?");
+// console.log("меня зовут " + name);
+
+//5
+// let voin = confirm("kto ty voin?");
+
+// if (voin) {
+//   console.log("ya voin.");
+// } else {
+//   console.log("ya ne voin.");
+// }
+
+//6
+// let password = prompt('password:')
+// if(password == 'nurs') {
+//     console.log('pravilno');
+// } else {
+//     console.log('ne pavilno');
+// }
+
+//7
+// let num = prompt('vedite chislo')
+// if (num %2 ===0) {
+//     console.log('chislo chetnoe');
+// } else{
+//     console.log('ne chetnoe');
+//      document.write("ne chetnoe");  // vivodit na ekran
+// }
+
+//8
+// let color = prompt("color?");
+// if (color == "red") {
+//   console.log("krasni zvet");
+// } else if (color == "black") {
+//   console.log("black");
+// } else if (color == "das") {
+//   console.log("dasasdsad");
+// } else {
+//   console.log("drugoi zwet");
+// }
+
+//9
+// let num = prompt("vedite chislo");
+// let kvadrd = num * num;
+// console.log(kvadrd);
+
+// //10
+// let grade = prompt("ozenka");
+// if (grade == 5) {
+//   console.log("molodez");
+// } else if (grade == 4) {
+//   console.log("xorosho");
+// } else if (grade == 3) {
+//   console.log("poidet");
+// } else {
+//   console.log("ploho");
+// }
